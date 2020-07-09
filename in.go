1c15c329ba95fed0cb57ca12cd42d0060ffbdd2f
cander <cander@htdtek.com>
2020-07-09 22:42:02 +0800

    增加in的支持

---
+++ in.go
@@ -0,0 +1,86 @@
+package gomysql
+
+import (
+	"database/sql"
+	"errors"
+	"reflect"
+	"strings"
+)
+
+var RangeOutErr = errors.New("索引值超出范围")
+
+func makeArgs(cmd string, args ...interface{}) (string, []interface{}, error) {
+	vs := make([]interface{}, 0)
+	var err error
+	for index, value := range args {
+		typ := reflect.TypeOf(value)
+		if typ.Kind() == reflect.Array || typ.Kind() == reflect.Slice {
+			vv := reflect.ValueOf(value)
+			cmd, err = replace(cmd, index, vv.Len())
+			if err != nil {
+				return cmd, vs, err
+			}
+			vs = append(vs, vv.Interface().([]interface{})...)
+		} else {
+			vs = append(vs, value)
+		}
+	}
+	return cmd, vs, nil
+}
+
+func replace(cmd string, index int, count int) (string, error) {
+	m := make([]string, count)
+	for j := 0; j < count; j++ {
+		m[j] = "?"
+	}
+
+	c := strings.Count(cmd, "?")
+	if index > c-1 {
+		return "", RangeOutErr
+	}
+	start := 0
+	tmp := cmd
+	for i := 0; i < c; i++ {
+		thisIndex := strings.Index(tmp, "?")
+		start = start + thisIndex + 1
+		tmp = tmp[thisIndex+1:]
+		if i == index {
+
+			return cmd[:start-1] + strings.Join(m, ",") + cmd[start:], nil
+		}
+	}
+
+	return cmd, nil
+}
+
+func (d *Db) UpdateIn(cmd string, args ...interface{}) (int64, error) {
+	newcmd, newargs, err := makeArgs(cmd, args...)
+	if err != nil {
+		return 0, err
+	}
+	return d.Update(newcmd, newargs...)
+}
+
+func (d *Db) DeleteIn(cmd string, args ...interface{}) (int64, error) {
+	newcmd, newargs, err := makeArgs(cmd, args...)
+	if err != nil {
+		return 0, err
+	}
+	return d.Delete(newcmd, newargs...)
+}
+
+func (d *Db) GetRowsIn(cmd string, args ...interface{}) (*sql.Rows, error) {
+	newcmd, newargs, err := makeArgs(cmd, args...)
+	if err != nil {
+		return nil, err
+	}
+	return d.GetRows(newcmd, newargs...)
+}
+
+func (d *Db) GetOneIn(cmd string, args ...interface{}) *sql.Row {
+	newcmd, newargs, err := makeArgs(cmd, args...)
+	if err != nil {
+		panic(err)
+	}
+	return d.GetOne(newcmd, newargs...)
+}
---
+++ in_test.go
@@ -0,0 +1,21 @@
+package gomysql
+
+import "testing"
+
+func TestIn(t *testing.T) {
+	cmd, args, err := makeArgs("select * from xxx where id=? and a in (?)", 6666, []interface{}{1, 2, 4, 5, 6, 7, 8, 89, 3, 4})
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(cmd)
+	t.Log(args)
+}
+
+func TestReplace(t *testing.T) {
+	// 讲索引为什么的？。 替换成n个
+	am, err := replace("select * from xxx where id=? and a in (?)", 1, 10)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(am)
+}
